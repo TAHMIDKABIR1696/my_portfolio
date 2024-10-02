1b4ae85c6e5de644b7e875cacd3e2bc8349746bd
tahmid <tahmidkabirp01814@gmail.com>
2024-10-02 11:56:00 +0600

    adding pandas and app titles

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas
 st.set_page_config(layout="wide")
+
+
 col1, col2 = st.columns(2)
 
 with col1:
@@ -7,6 +10,20 @@ with col1:
 
 with col2:
     st.title("MD.TAHMID KABIR")
-    myinfo = "Hi this is my project. Welcome inside the webpage. May you like it"
+    myinfo = "Hi this is my project."
 
     st.info(myinfo)
+
+myinfo2 = "Wellcome and feel free to explore the website"
+st.info(myinfo2)
+
+df = pandas.read_csv("data.csv",sep=";")
+col3, col4 = st.columns(2)
+with col3:
+    for index , row in df[:10].iterrows():
+        st.header(row["title"])
+
+
+with col4:
+    for index , row in df[10:].iterrows():
+        st.header(row["title"])
